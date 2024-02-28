99df1dde19c06c4dfbcbbbde7a40469dbc5c8589
Adenola Omotayo <adenolaomotayo@gmail.com>
2024-02-28 12:42:55 +0100

    initial commit

--- src/Features/Home/Header.jsx
+++ src/Features/Home/Header.jsx
@@ -1,23 +1,27 @@
 import p9 from "../../images/logo 1.png";
 function Header() {
   return (
-    <div className="grid-2 m-g">
-      <div className="grid-2-cols m-t">
-        <h1 className=" text-header">
-          Welcome To Vertiline Synergy Training School.
-        </h1>
-        <p className="header-text">
-          Start, switch, or advance your career with over # courses,
-          Professional Certificates which has global recognition from reputable
-          and qualified tutors
-        </p>
+    <>
+      <div className="container">
+        <div className="grid-2 m-g">
+          <div className="grid-2-cols m-t">
+            <h1 className=" text-header">
+              Welcome To Vertiline Synergy Training School.
+            </h1>
+            <p className="header-text">
+              Start, switch, or advance your career with over # courses,
+              Professional Certificates which has global recognition from
+              reputable and qualified tutors
+            </p>
 
-        <button className="join">Get Started</button>
+            <button className="join">Get Started</button>
+          </div>
+        <div className="grid-2-cols header-image">
+          <img src={p9} alt="" className="header-img hidden" />
+        </div>
       </div>
-      <div className="grid-2-cols">
-        <img src={p9} alt="" className="header-img" />
-      </div>
-    </div>
+        </div>
+    </>
   );
 }
 
